73b13b5209508d3c92c9b39cbf42bb4e1f1b4d12
elianfloresp <elianfelipe.flores@gmail.com>
2023-08-28 20:08:07 -0300

    feat: settings in js

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let scene = 1;
 
     const showCurrentScene = (scene) => {
         const sceneElements = document.querySelectorAll(".scene");
-        
+
         sceneElements.forEach(element => {
             element.classList.add("hide");
         });
@@ -16,16 +16,43 @@ document.addEventListener("DOMContentLoaded", function() {
     const previousButtons = document.querySelectorAll(".js-btn-previous");
 
     nextButtons.forEach(button => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             scene++;
             showCurrentScene(scene);
         });
     });
 
     previousButtons.forEach(button => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             scene--;
             showCurrentScene(scene);
         });
     });
 });
+
+
+const checkboxes = document.querySelectorAll('.checkbox-option');
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            checkboxes.forEach(otherCheckbox => {
+                if (otherCheckbox !== checkbox) {
+                    otherCheckbox.checked = false;
+                }
+            });
+        }
+    });
+});
+
+const textarea = document.getElementById('loss');
+    const charCount = document.getElementById('counter');
+
+    textarea.addEventListener('input', () => {
+        const currentLength = textarea.value.length;
+        charCount.textContent = `${currentLength}/130 characters`;
+
+        if (currentLength > 130) {
+            textarea.value = textarea.value.slice(0, 130);
+        }
+    });
